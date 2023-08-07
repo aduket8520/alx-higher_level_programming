3f189ed45a6fda8cd0afdd2399308697ca1777e8
titus <titusaduket@gmail.com>
2023-08-08 01:07:48 +0300

    this is the whole

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+wordFirst = word[:3]
+wordLast = word[-2:]
+middleWord = word[1:-1]
+print("First 3 letters: {}".format(wordFirst))
+print("Last 2 letters: {}".format(wordLast))
+print("Middle word: {}".format(middleWord))
