f5f78c3f240fca42f3640d7494f67e81c6138c88
titus <titusaduket@gmail.com>
2023-08-30 00:02:59 +0300

    initila file addition

--- 0x05-python-exceptions/1-safe_print_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        print()
-        return True
-    except IndexError:
-        return False
