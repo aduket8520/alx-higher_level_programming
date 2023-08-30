93d0f9292f731813ebfeff664d688b12eb97b47b
titus <titusaduket@gmail.com>
2023-08-30 12:51:49 +0300

    edited for readability

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """print the elements in the list
+
+    Args:
+        my_list (list): the list containing the data
+        x (int): integer for date in the list
+
+    Returns:
+        The number of elements printed
+    """
     count = 0
     for i in range(x):
         try:
