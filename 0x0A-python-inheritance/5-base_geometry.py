20d045199e0c12eec3d526228ea0a578d116972a
titus <titusaduket@gmail.com>
2023-09-13 23:02:47 +0300

    indented the base geometry file

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an empty class BaseGeometry."""
 
+
 class BaseGeometry:
-  """this is an empty class BaseGeometry"""
-  pass
+    """this is an empty class BaseGeometry"""
+    pass
